35e6edbdee8d9b2e62110ac1fc17c029a684bdd6
Scott Stevenson <scott@1box.onmicrosoft.com>
2022-08-30 14:08:33 -0700

    Updatin’ GitHub Pages

---
+++ assets/HomeLink.1c8331e3.js
@@ -0,0 +1,15 @@
+import { a as jsx } from "./index.a364b5af.js";
+import { L as LinkedSVG } from "./LinkedSVG.c5604b3e.js";
+const HomeLink = () => /* @__PURE__ */ jsx(LinkedSVG, {
+  href: "/",
+  svg: "../favicon.svg",
+  h: "3rem",
+  w: "2rem",
+  position: "fixed",
+  left: "0.25rem",
+  top: "0.25rem"
+});
+export {
+  HomeLink as H
+};
+//# sourceMappingURL=HomeLink.1c8331e3.js.map
