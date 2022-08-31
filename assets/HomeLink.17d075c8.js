7a7a3d151cf922ffa6a6516739bd3d774bb308a5
W.J. “dysbulic” H <dys@dhappy.org>
2022-08-30 23:59:42 -0400

    Updatin’ GitHub Pages

---
+++ assets/HomeLink.17d075c8.js
@@ -0,0 +1,15 @@
+import { a as jsx } from "./index.cf1ff3b8.js";
+import { L as LinkedSVG } from "./LinkedSVG.282ec1ea.js";
+const HomeLink = () => /* @__PURE__ */ jsx(LinkedSVG, {
+  href: "/",
+  svg: "../favicon.svg",
+  h: "3rem",
+  w: "2rem",
+  position: "fixed",
+  left: "0.25rem",
+  top: "0.25rem"
+});
+export {
+  HomeLink as H
+};
+//# sourceMappingURL=HomeLink.17d075c8.js.map
